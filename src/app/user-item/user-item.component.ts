75ebd47c52b9e65c6750bbdacfa0a6580c0d66cb
ByakuyaKuchiki <rtsiry@live.fr>
2018-01-20 21:59:45 +0400

    passing input

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class UserItemComponent implements OnInit {
 
   name: string; // new property
+
+  @Input() nameInput: string; // accepted input
 
   constructor() { 
     this.name = 'Felipe'; // set property value
